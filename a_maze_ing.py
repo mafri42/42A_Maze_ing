db151d80c57ad684d869f43fb615567643b2ac40
Annalisa Castaldo <acastald@c2r1p5.42firenze.it>
2026-07-06 16:52:05 +0200

    io non ci capisco niente

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -2,11 +2,12 @@ import sys
 from src.a_maze_ing.parsing import config
 from src.a_maze_ing.maze_generator import MazeGenerator
 from src.a_maze_ing.maze_render import MazeRenderer
+from src.a_maze_ing.ft_pattern import Pattern
 
 
 if __name__ == "__main__":
     data = config()
-    maze = MazeGenerator(
+    maze = Pattern(
         width=data['width'],
         height=data['height'],
         entry=data['entry'],
@@ -23,7 +24,7 @@ if __name__ == "__main__":
         sys.exit(1)
     print(f"\nLab {maze.width}x{maze.height} initialized!")
     try:
-        renderer = MazeRenderer(maze)
+        renderer = MazeRenderer(maze, maze.pattern_ft)
         renderer.interactive_menu()
     except ValueError as e:
         print(f"Error: {e}")
--- src/a_maze_ing/maze_render.py
+++ src/a_maze_ing/maze_render.py
@@ -1,10 +1,12 @@
+from .ft_pattern import Pattern
 import os
 
+
 class MazeRenderer:
-    def __init__(self, maze_gen):
+    def __init__(self, maze_gen, ft_coords) -> None:
         self.maze = maze_gen
         self.show_path = False
-        
+
         self.WALL_COLORS = [
             "\033[37m",  # 0: Bianco
             "\033[36m",  # 1: Ciano
@@ -19,10 +21,10 @@ class MazeRenderer:
         self.ENTRY_COLOR = "\033[45m"  # Sfondo Magenta per l'ingresso
         self.EXIT_COLOR = "\033[41m"   # Sfondo Rosso per l'uscita
         self.PATH_COLOR = "\033[44m"   # Sfondo Blu per il cammino
-        self.FORTY_TWO_COLOR = "\033[42m" # Sfondo Verde per le celle del 42
+        self.FORTY_TWO_COLOR = "\033[42m"  # Sfondo Verde per le celle del 42
 
-        self.path_coords = [] 
-        self.forty_two_coords = [] 
+        self.ft_coords = ft_coords
+        self.path_coords = False
 
     def clear_screen(self):
         os.system('cls' if os.name == 'nt' else 'clear')
@@ -30,47 +32,35 @@ class MazeRenderer:
     def draw(self):
         matrix = self.maze.matrix
         wall_color = self.WALL_COLORS[self.current_color_idx]
-        
         for y, row in enumerate(matrix):
             top = ""
             col = ""
-            
             for x, cell in enumerate(row):
-                # 1 Tetto (Nord - Bit 0)
                 if cell & 1:
                     top += f"{wall_color}+───{self.RESET}"
                 else:
                     top += f"{wall_color}+{self.RESET}   "
-                
-                # 2 Muro Sinistro (Ovest - Bit 3)
                 if cell & 8:
                     col += f"{wall_color}│{self.RESET}"
                 else:
                     col += " "
-                
-                # 3 Interno della Cella
                 if (x, y) == self.maze.entry:
                     col += f"{self.ENTRY_COLOR}   {self.RESET}"
                 elif (x, y) == self.maze.exit:
                     col += f"{self.EXIT_COLOR}   {self.RESET}"
                 elif self.show_path and (x, y) in self.path_coords:
                     col += f"{self.PATH_COLOR}   {self.RESET}"
-                elif (x, y) in self.forty_two_coords:
+                elif (x, y) in self.ft_coords:
                     col += f"{self.FORTY_TWO_COLOR}   {self.RESET}"
                 else:
-                    col += "   " # Spazio vuoto
-            
-            # bordo destro di ogni riga
+                    col += "   "
             top += f"{wall_color}+{self.RESET}"
-            if row[-1] & 2: # Est (Bit 1) dell'ultima cella
+            if row[-1] & 2:  # Est (Bit 1) dell'ultima cella
                 col += f"{wall_color}│{self.RESET}"
             else:
                 col += " "
-                
             print(top)
             print(col)
-            
-        # 4 Pavimento Finale (Sud - Bit 2 dell'ultima riga)
         bot = ""
         for cell in matrix[-1]:
             if cell & 4:
@@ -84,15 +74,12 @@ class MazeRenderer:
         while True:
             self.clear_screen()
             self.draw()
-            
             print("\n=== A-Maze-ing ===")
             print("1. Re-generate a new maze")
             print("2. Show/Hide path from entry to exit")
             print("3. Change maze wall colors")
             print("4. Quit")
-            
             choice = input("Choice? (1-4): ")
-            
             if choice == '1': 
                 self.maze.matrix = [[15 for _ in range(self.maze.width)] for _ in range(self.maze.height)]
                 self.maze.mazing()
